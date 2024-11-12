ec4fcddc51140c92aa830a630a01b07105acc609
git-innit <187617015+git-innit@users.noreply.github.com>
2024-11-12 13:01:39 +0100

    Add Block-03 Aufgabe-04

---
+++ Block-03/Aufgabe-04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aufgabe_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dezimalZahl = DezimalzahlEinlesen(0, 99999);
+            string binaerZahl = BerechneBinaerZahl(dezimalZahl);
+            Console.Write("Binaerzahl :");
+            for (int i = binaerZahl.Length - 1; i >= 0; i--)
+                Console.Write(binaerZahl[i]);
+            Console.ReadLine();
+        }
+        private static int DezimalzahlEinlesen(int min, int max)
+        {
+            int dezimalZahl = 0;
+            do
+            {
+                Console.Write("Dezimalzahl von {0} bis {1}:",min,max);
+            } while (int.TryParse(Console.ReadLine(), out dezimalZahl) == false || dezimalZahl < min || dezimalZahl > max);
+            return dezimalZahl;
+        }
+        private static string BerechneBinaerZahl(int dezimalZahl)
+        {
+            string binaerZahl = "";
+            for (int dz = dezimalZahl; dz >= 1; dz /= 2)
+                binaerZahl = binaerZahl + Convert.ToString(dz % 2);
+            return binaerZahl;
+        }
+    }
+}
