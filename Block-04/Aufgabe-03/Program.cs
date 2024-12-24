3a3133810130abd95d8a3c31d14b47be46b1a23d
git-innit <187617015+git-innit@users.noreply.github.com>
2024-12-24 02:10:12 +0100

    Add Block-04 Aufgabe-03

---
+++ Block-04/Aufgabe-03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aufgabe_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Startwert? :\t");
+            string startwert = Console.ReadLine();
+            Console.Write("Endwert? :\t");
+            string endwert = Console.ReadLine();
+            int x = Convert.ToInt32(startwert);
+            Console.WriteLine(x);
+            while (x <= Convert.ToInt32(endwert))
+            {
+                x += 1;
+                Console.WriteLine(x);
+            }
+            Environment.Exit(0);
+        }
+    }
+}
