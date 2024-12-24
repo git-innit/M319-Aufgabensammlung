9203c397645b8b55e9ad34ab0355e26f1b8955b3
git-innit <187617015+git-innit@users.noreply.github.com>
2024-12-24 02:56:42 +0100

    Add Block-04 Aufgabe-04

---
+++ Block-04/Aufgabe-04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Aufgabe_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte Maximalwert eingeben:\t");
+            int maximalwert = Convert.ToInt32(Console.ReadLine());
+
+            if (maximalwert <= 2)
+            {
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine(2);
+            int zahl = 3;
+
+            while (zahl <= maximalwert)
+            {
+                int check = 2;
+                bool istNichtModNull = true;
+
+                while (check <= Math.Sqrt(zahl) + 1)
+                {
+                    if (zahl % check == 0)
+                    {
+                        istNichtModNull = false;
+                        break;
+                    }
+                    check += 1;
+                }
+
+                if (istNichtModNull)
+                {
+                    Console.WriteLine(zahl);
+                }
+
+                zahl += 2;
+            }
+            Environment.Exit(0);
+        }
+    }
+}
