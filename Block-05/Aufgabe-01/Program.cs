b1895ba69bacb8443a4bec93d07029fefac2ccdc
git-innit <187617015+git-innit@users.noreply.github.com>
2025-01-14 09:18:09 +0100

    Add Block-05 Aufgabe-01

---
+++ Block-05/Aufgabe-01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Aufgabe_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Wert {i + 1}?:\t");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("\n" + array.Sum());
+        }
+    }
+}
